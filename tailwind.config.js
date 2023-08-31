a5093894b7bf2c449183cb23b75b8c7a98a36609
anishazahan <anishazahan13@gmail.com>
2023-08-31 19:08:18 +0600

    design login and register component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6b21a8",
+        graye: "#d1fae5",
+      },
+    },
   },
   plugins: [],
 };
