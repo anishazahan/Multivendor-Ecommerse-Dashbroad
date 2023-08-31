4d6d05959875d3d8f1dc5e3230d8b25c0c28f93c
anishazahan <anishazahan13@gmail.com>
2023-08-31 19:40:46 +0600

    admin login page design

--- src/router/routes/PublicRoute.js
+++ src/router/routes/PublicRoute.js
@@ -1,10 +1,8 @@
 import { lazy } from "react";
-// import Login from "../../views/auth/Login";
-// import Register from "../../views/auth/Register";
 
 const Login = lazy(() => import("../../views/auth/Login.jsx"));
 const Register = lazy(() => import("../../views/auth/Register.jsx"));
-// const AdminLogin = lazy(() => import('../../views/auth/AdminLogin'))
+const AdminLogin = lazy(() => import("../../views/auth/AdminLogin"));
 // const Home = lazy(() => import('../../views/Home'))
 // const UnAuthorized = lazy(() => import('../../views/UnAuthorized'))
 const publicRoutes = [
@@ -20,13 +18,13 @@ const publicRoutes = [
     path: "/register",
     element: <Register />,
   },
+  {
+    path: "/admin/login",
+    element: <AdminLogin />,
+  },
   // {
-  //     path: '/admin/login',
-  //     element: <AdminLogin />
+  //   path: "/unauthorized",
+  //   element: <UnAuthorized />,
   // },
-  // {
-  //     path: '/unauthorized',
-  //     element: <UnAuthorized />
-  // }
 ];
 export default publicRoutes;
