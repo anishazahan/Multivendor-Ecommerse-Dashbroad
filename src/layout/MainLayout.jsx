653efbf8f8dc0d8cd31524a50896d1dd19940ad6
anishazahan <anishazahan13@gmail.com>
2023-09-04 09:16:22 +0600

    main layout component design

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./LeftSidebar";
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <div className="bg-gray-100 w-full min-h-screen">
+      <Header />
+      <Sidebar />
+      <div className="ml-0 lg:ml-[260px] pt-[95px] transition-all">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
