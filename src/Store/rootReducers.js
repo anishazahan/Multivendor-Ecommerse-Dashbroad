f6554bb4520b60f9df866bd15123dc9bf2b0749e
anishazahan <anishazahan13@gmail.com>
2023-09-13 09:48:34 +0600

    chat reducer create

--- src/Store/rootReducers.js
+++ src/Store/rootReducers.js
@@ -1,16 +1,16 @@
 import authReducer from "./Reducers/authReducer";
 import categoryReducer from "./Reducers/categoryReducer";
+import chatReducer from "./Reducers/chatReducer";
 import productReducer from "./Reducers/productReducer";
 import sellerReducer from "./Reducers/sellerReducer";
 
-// import chatReducer from './Reducers/chatReducer'
 // import OrderReducer from './Reducers/OrderReducer'
 const rootReducer = {
   auth: authReducer,
   category: categoryReducer,
   product: productReducer,
   seller: sellerReducer,
-  // chat: chatReducer,
+  chat: chatReducer,
   // order: OrderReducer
 };
 export default rootReducer;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,39 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./LeftSidebar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { socket } from "../utils/utils";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  activeStatus_update,
+  updateCustomer,
+  updateSellers,
+} from "../Store/Reducers/chatReducer";
 
 const MainLayout = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo && userInfo.role === "seller") {
+      socket.emit("add_seller", userInfo._id, userInfo);
+    } else {
+      socket.emit("add_admin", userInfo);
+    }
+  }, [userInfo]);
+
+  useEffect(() => {
+    socket.on("activeCustomer", (customers) => {
+      dispatch(updateCustomer(customers));
+    });
+    socket.on("activeSeller", (sellers) => {
+      dispatch(updateSellers(sellers));
+    });
+    socket.on("activeAdmin", (data) => {
+      dispatch(activeStatus_update(data));
+    });
+  }, []);
   return (
     <div className="bg-gray-100 w-full min-h-screen">
       <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
