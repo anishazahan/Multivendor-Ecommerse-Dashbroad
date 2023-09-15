e23d71ceb1cb352b62fa57f4eb6707117a2d6539
anishazahan <anishazahan13@gmail.com>
2023-09-15 08:07:50 +0600

    get orders for admin

--- src/Store/rootReducers.js
+++ src/Store/rootReducers.js
@@ -1,16 +1,16 @@
 import authReducer from "./Reducers/authReducer";
 import categoryReducer from "./Reducers/categoryReducer";
 import chatReducer from "./Reducers/chatReducer";
+import OrderReducer from "./Reducers/orderReducer";
 import productReducer from "./Reducers/productReducer";
 import sellerReducer from "./Reducers/sellerReducer";
 
-// import OrderReducer from './Reducers/OrderReducer'
 const rootReducer = {
   auth: authReducer,
   category: categoryReducer,
   product: productReducer,
   seller: sellerReducer,
   chat: chatReducer,
-  // order: OrderReducer
+  order: OrderReducer,
 };
 export default rootReducer;
--- src/views/pages/admin/Orders.jsx
+++ src/views/pages/admin/Orders.jsx
@@ -1,50 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { Link } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { get_admin_orders } from "../../../Store/Reducers/orderReducer";
 import Pagination from "../../Pagination";
-// import Pagination from "../Pagination";
-// import { useSelector, useDispatch } from "react-redux";
-// import { get_admin_orders } from "../../store/Reducers/OrderReducer";
 
 const Orders = () => {
-  //   const dispatch = useDispatch();
-  //   const { totalOrder, myOrders } = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+  const { totalOrder, myOrders } = useSelector((state) => state.order);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const [parPage, setParPage] = useState(5);
   const [show, setShow] = useState("");
 
-  //   useEffect(() => {
-  //     dispatch(
-  //       get_admin_orders({
-  //         parPage: parseInt(parPage),
-  //         page: parseInt(currentPage),
-  //         searchValue,
-  //       })
-  //     );
-  //   }, [parPage, currentPage, searchValue]);
+  useEffect(() => {
+    dispatch(
+      get_admin_orders({
+        parPage: parseInt(parPage),
+        page: parseInt(currentPage),
+        searchValue,
+      })
+    );
+  }, [parPage, currentPage, searchValue]);
 
   return (
-    <div className="px-2 lg:px-5 pt-3">
-      <div className="w-full p-4  bg-white shadow rounded-md">
+    <div className="px-2 lg:px-7 pt-5">
+      <div className="w-full p-4  bg-[#283046] rounded-md">
         <div className="flex justify-between items-center">
           <select
             onChange={(e) => setParPage(parseInt(e.target.value))}
-            className="px-4 py-2 focus:border-purple-500 outline-none bg-white shadow border border-slate-200 rounded-md text-gray-700"
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]"
           >
             <option value="5">5</option>
             <option value="5">15</option>
             <option value="5">25</option>
           </select>
           <input
-            className="px-4 py-2 focus:border-indigo-200 outline-none bg-gray-50 border border-slate-100 rounded-md text-gray-400"
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]"
             type="text"
             placeholder="search"
           />
         </div>
         <div className="relative mt-5 overflow-x-auto">
           <div className="w-full text-sm text-left [#d0d2d6]">
-            <div className="text-sm text-gray-800 uppercase border-b border-slate-200">
+            <div className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
               <div className="flex justify-between items-start">
                 <div className="py-3 w-[25%]">Order Id</div>
                 <div className="py-3 w-[13%]">Price</div>
@@ -56,64 +56,62 @@ const Orders = () => {
                 </div>
               </div>
             </div>
-            {/* {myOrders.map((o, i) => ( */}
-            <div className="text-gray-500">
-              <div className="flex justify-between items-start border-b border-slate-200">
-                <div className="py-4 w-[25%] font-medium whitespace-nowrap">
-                  {/* {o._id} */}
-                  id
-                </div>
-                {/* <div className="py-4 w-[13%]">${o.price}</div>
+            {myOrders.map((o, i) => (
+              <div className="text-[#d0d2d6]">
+                <div className="flex justify-between items-start border-b border-slate-700">
+                  <div className="py-4 w-[25%] font-medium whitespace-nowrap">
+                    {o._id}
+                  </div>
+                  <div className="py-4 w-[13%]">${o.price}</div>
                   <div className="py-4 w-[18%]">{o.payment_status}</div>
                   <div className="py-4 w-[18%]">{o.delivery_status}</div>
                   <div className="py-4 w-[18%]">
                     <Link to={`/admin/dashboard/order/details/${o._id}`}>
                       view
                     </Link>
-                  </div> */}
+                  </div>
+                  <div
+                    onClick={(e) => setShow(o._id)}
+                    className="py-4 cursor-pointer w-[8%]"
+                  >
+                    <MdKeyboardArrowDown />
+                  </div>
+                </div>
                 <div
-                  // onClick={(e) => setShow(o._id)}
-                  className="py-4 cursor-pointer w-[8%]"
+                  className={
+                    show === o._id
+                      ? "block border-b border-slate-700 bg-slate-800"
+                      : "hidden"
+                  }
                 >
-                  <MdKeyboardArrowDown />
-                </div>
-              </div>
-              <div
-              //   className={
-              //     show === o._id
-              //       ? "block border-b border-slate-200 bg-slate-100"
-              //       : "hidden"
-              //   }
-              >
-                {/* {o.suborder.map((so, i) => ( */}
-                <div className="flex justify-start items-start border-b border-slate-200">
-                  {/* <div className="py-4 w-[25%] font-medium whitespace-nowrap pl-3">
-                      ${so._id}
+                  {o.suborder.map((so, i) => (
+                    <div className="flex justify-start items-start border-b border-slate-700">
+                      <div className="py-4 w-[25%] font-medium whitespace-nowrap pl-3">
+                        ${so._id}
+                      </div>
+                      <div className="py-4 w-[13%]">${so.price}</div>
+                      <div className="py-4 w-[18%]">{so.payment_status}</div>
+                      <div className="py-4 w-[18%]">{so.delivery_status}</div>
                     </div>
-                    <div className="py-4 w-[13%]">${so.price}</div>
-                    <div className="py-4 w-[18%]">{so.payment_status}</div>
-                    <div className="py-4 w-[18%]">{so.delivery_status}</div> */}
+                  ))}
                 </div>
-                {/* ))} */}
               </div>
-            </div>
-            {/* ))} */}
+            ))}
           </div>
         </div>
-        {/* {totalOrder <= parPage ? (
+        {totalOrder <= parPage ? (
           ""
-        ) : ( */}
-        <div className="w-full flex justify-end mt-4 bottom-4 right-4">
-          <Pagination
-            pageNumber={currentPage}
-            setPageNumber={setCurrentPage}
-            //   totalItem={totalOrder}
-            totalItem={50}
-            parPage={parPage}
-            showItem={4}
-          />
-        </div>
-        {/* )} */}
+        ) : (
+          <div className="w-full flex justify-end mt-4 bottom-4 right-4">
+            <Pagination
+              pageNumber={currentPage}
+              setPageNumber={setCurrentPage}
+              totalItem={totalOrder}
+              parPage={parPage}
+              showItem={4}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
