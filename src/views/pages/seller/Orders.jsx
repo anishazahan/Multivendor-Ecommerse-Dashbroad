6f63d2bbe25cd9e0e0efc8e7b4e1d6b1db936ff2
anishazahan <anishazahan13@gmail.com>
2023-09-17 17:34:05 +0600

    get order for seller

--- src/views/pages/seller/Orders.jsx
+++ src/views/pages/seller/Orders.jsx
@@ -5,25 +5,26 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Pagination from "../../Pagination";
 import Search from "../../components/Search";
-// import { get_seller_orders } from "../../store/Reducers/OrderReducer";
+import { get_seller_orders } from "../../../Store/Reducers/orderReducer";
+
 const Orders = () => {
   const dispatch = useDispatch();
-  //   const { totalOrder, myOrders } = useSelector((state) => state.order);
-  //   const { userInfo } = useSelector((state) => state.auth);
+  const { totalOrder, myOrders } = useSelector((state) => state.order);
+  const { userInfo } = useSelector((state) => state.auth);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const [parPage, setParPage] = useState(5);
 
-  //   useEffect(() => {
-  //     dispatch(
-  //       get_seller_orders({
-  //         parPage: parseInt(parPage),
-  //         page: parseInt(currentPage),
-  //         searchValue,
-  //         sellerId: userInfo._id,
-  //       })
-  //     );
-  //   }, [parPage, currentPage, searchValue]);
+  useEffect(() => {
+    dispatch(
+      get_seller_orders({
+        parPage: parseInt(parPage),
+        page: parseInt(currentPage),
+        searchValue,
+        sellerId: userInfo._id,
+      })
+    );
+  }, [parPage, currentPage, searchValue]);
 
   return (
     <div className="px-2 lg:px-5 pt-1 ">
@@ -58,7 +59,7 @@ const Orders = () => {
               </tr>
             </thead>
             <tbody>
-              {/* {myOrders.map((d, i) => (
+              {myOrders.map((d, i) => (
                 <tr key={i}>
                   <td
                     scope="row"
@@ -102,24 +103,24 @@ const Orders = () => {
                     </Link>
                   </td>
                 </tr>
-              ))} */}
+              ))}
             </tbody>
           </table>
         </div>
-        {/* {totalOrder <= parPage ? (
+        {totalOrder <= parPage ? (
           ""
-        ) : ( */}
-        <div className="w-full flex justify-end mt-4 bottom-4 right-4">
-          <Pagination
-            pageNumber={currentPage}
-            setPageNumber={setCurrentPage}
-            //   totalItem={totalOrder}
-            totalItem={50}
-            parPage={parPage}
-            showItem={3}
-          />
-        </div>
-        {/* )} */}
+        ) : (
+          <div className="w-full flex justify-end mt-4 bottom-4 right-4">
+            <Pagination
+              pageNumber={currentPage}
+              setPageNumber={setCurrentPage}
+              totalItem={totalOrder}
+              // totalItem={50}
+              parPage={parPage}
+              showItem={3}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
