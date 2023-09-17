991a45d91da2a8b2bf33ada78fa1bac904c6afb2
anishazahan <anishazahan13@gmail.com>
2023-09-18 01:10:32 +0600

    get dashbroad data for seller

--- src/views/pages/admin/AdminDashboard.jsx
+++ src/views/pages/admin/AdminDashboard.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { RiProductHuntLine } from "react-icons/ri";
 import { FaUsers } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import Chart from "react-apexcharts";
+import moment from "moment";
+import { useSelector, useDispatch } from "react-redux";
+import seller from "../../../assets/success.png";
+import { get_admin_dashboard_index_data } from "../../../Store/Reducers/dashboardIndexReducer";
+
 const AdminDashboard = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const {
+    totalSale,
+    totalOrder,
+    totalProduct,
+    totalSeller,
+    recentOrders,
+    recentMessage,
+  } = useSelector((state) => state.dashboardIndex);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(get_admin_dashboard_index_data());
+  }, []);
+
   const state = {
     series: [
       {
@@ -22,13 +43,13 @@ const AdminDashboard = () => {
       },
     ],
     options: {
-      color: ["#0f766e", "#57534e"],
+      color: ["#181ee8", "#181ee8"],
       plotOptions: {
         radius: 30,
       },
       chart: {
         background: "transparent",
-        foreColor: "rgba(0,0,0,0.7)",
+        foreColor: "#d0d2d6",
       },
       dataLabels: {
         enabled: false,
@@ -37,7 +58,7 @@ const AdminDashboard = () => {
         show: true,
         curve: ["smooth", "straight", "stepline"],
         lineCap: "butt",
-        colors: "rgba(0,0,0,0.7)",
+        colors: "#f0f0f0",
         width: 0.5,
         dashArray: 0,
       },
@@ -94,41 +115,39 @@ const AdminDashboard = () => {
     },
   };
   return (
-    <div className="px-2 md:px-5 pb-3">
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5">
-        <div className="flex justify-between items-center p-5 bg-white shadow rounded-md gap-3">
-          <div className="flex flex-col justify-start items-start text-gray-900">
-            <h2 className="text-3xl font-bold">$6566</h2>
-            <span className="text-md font-medium text-gray-500">
-              Total Sales
-            </span>
+    <div className="px-2 md:px-7 py-5">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7">
+        <div className="flex justify-between items-center p-5 bg-[#283046] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#d0d2d6]">
+            <h2 className="text-3xl font-bold">${totalSale}</h2>
+            <span className="text-md font-medium">Total Sales</span>
           </div>
           <div className="w-[46px] h-[47px] rounded-full bg-[#28c76f1f] flex justify-center items-center text-xl">
             <BsCurrencyDollar className="text-[#28c76f] shadow-lg" />
           </div>
         </div>
-        <div className="flex justify-between items-center p-5 bg-white shadow rounded-md gap-3">
-          <div className="flex flex-col justify-start items-start text-gray-900">
-            <h2 className="text-3xl font-bold">20</h2>
-            <span className="text-md font-medium text-gray-500">Products</span>
+        <div className="flex justify-between items-center p-5 bg-[#283046] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#d0d2d6]">
+            <h2 className="text-3xl font-bold">{totalProduct}</h2>
+            <span className="text-md font-medium">Products</span>
           </div>
           <div className="w-[46px] h-[47px] rounded-full bg-[#e000e81f] flex justify-center items-center text-xl">
             <RiProductHuntLine className="text-[#cd00e8] shadow-lg" />
           </div>
         </div>
-        <div className="flex justify-between items-center p-5 bg-white shadow rounded-md gap-3">
-          <div className="flex flex-col justify-start items-start text-gray-900">
-            <h2 className="text-3xl font-bold">50</h2>
-            <span className="text-md font-medium text-gray-500">Sellers</span>
+        <div className="flex justify-between items-center p-5 bg-[#283046] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#d0d2d6]">
+            <h2 className="text-3xl font-bold">{totalSeller}</h2>
+            <span className="text-md font-medium">Sellers</span>
           </div>
           <div className="w-[46px] h-[47px] rounded-full bg-[#00cfe81f] flex justify-center items-center text-xl">
             <FaUsers className="text-[#00cfe8] shadow-lg" />
           </div>
         </div>
-        <div className="flex justify-between items-center p-5 bg-white shadow rounded-md gap-3">
-          <div className="flex flex-col justify-start items-start text-gray-900">
-            <h2 className="text-3xl font-bold">12</h2>
-            <span className="text-md font-medium text-gray-500">Orders</span>
+        <div className="flex justify-between items-center p-5 bg-[#283046] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#d0d2d6]">
+            <h2 className="text-3xl font-bold">{totalOrder}</h2>
+            <span className="text-md font-medium">Orders</span>
           </div>
           <div className="w-[46px] h-[47px] rounded-full bg-[#7367f01f] flex justify-center items-center text-xl">
             <AiOutlineShoppingCart className="text-[#7367f0] shadow-lg" />
@@ -137,7 +156,7 @@ const AdminDashboard = () => {
       </div>
       <div className="w-full flex flex-wrap mt-7">
         <div className="w-full lg:w-7/12 lg:pr-3">
-          <div className="w-full bg-white shadow px-4 pt-4 pb-6 rounded-md">
+          <div className="w-full bg-[#283046] p-4 rounded-md">
             <Chart
               options={state.options}
               series={state.series}
@@ -147,92 +166,64 @@ const AdminDashboard = () => {
           </div>
         </div>
         <div className="w-full lg:w-5/12 lg:pl-4 mt-6 lg:mt-0">
-          <div className="w-full bg-white p-4 shadow rounded-md text-gray-800">
-            <div className="flex justify-between items-center pb-3">
-              <h2 className="font-semibold text-gray-800">
+          <div className="w-full bg-[#283046] p-4 rounded-md text-[#d0d2d6]">
+            <div className="flex justify-between items-center">
+              <h2 className="font-semibold text-lg text-[#d0d2d6] pb-3">
                 Recent seller message
               </h2>
-              <Link className="font-semibold text-sm text-gray-700">
+              <Link className="font-semibold text-sm text-[#d0d2d6]">
                 View All
               </Link>
             </div>
-            <div className="flex flex-col gap-2 pt-6 text-gray-900">
-              <ol className="relative border-1 border-slate-300 ml-4">
-                <li className="mb-3 ml-6">
-                  <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#00d1e848] rounded-full z-10">
-                    <img
-                      className="w-full rounded-full h-full shadow-lg"
-                      src="http://localhost:3000/images/admin.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-3 bg-slate-100 rounded-lg border border-slate-200 shadow-sm">
-                    <div className="flex justify-between items-center mb-2">
-                      <Link className="text-md font-normal">Admin</Link>
-                      <time className="mb-1 text-sm font-normal sm:order-last sm:mb-0">
-                        4 day ago
-                      </time>
+            <div className="flex flex-col gap-2 pt-6 text-[#d0d2d6]">
+              <ol className="relative border-1 border-slate-600 ml-4">
+                {recentMessage.map((m, i) => (
+                  <li className="mb-3 ml-6">
+                    <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#00d1e848] rounded-full z-10">
+                      {m.senderId === userInfo._id ? (
+                        <img
+                          className="w-full rounded-full h-full shadow-lg"
+                          src={userInfo.image}
+                          alt=""
+                        />
+                      ) : (
+                        <img
+                          className="w-full rounded-full h-full shadow-lg"
+                          src={seller}
+                          alt=""
+                        />
+                      )}
                     </div>
-                    <div className="p-2 text-xs font-normal bg-slate-200 rounded-lg border border-slate-100">
-                      how are you
+                    <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
+                      <div className="flex justify-between items-center mb-2">
+                        <Link className="text-md font-normal">
+                          {m.senderName}
+                        </Link>
+                        <time className="mb-1 text-sm font-normal sm:order-last sm:mb-0">
+                          {moment(m.createdAt).startOf("hour").fromNow()}
+                        </time>
+                      </div>
+                      <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
+                        {m.message}
+                      </div>
                     </div>
-                  </div>
-                </li>
-                <li className="mb-3 ml-6">
-                  <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#00d1e848] rounded-full z-10">
-                    <img
-                      className="w-full rounded-full h-full shadow-lg"
-                      src="http://localhost:3000/images/admin.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-3 bg-slate-100 rounded-lg border border-slate-200 shadow-sm">
-                    <div className="flex justify-between items-center mb-2">
-                      <Link className="text-md font-normal">Admin</Link>
-                      <time className="mb-1 text-sm font-normal sm:order-last sm:mb-0">
-                        4 day ago
-                      </time>
-                    </div>
-                    <div className="p-2 text-xs font-normal bg-slate-200 rounded-lg border border-slate-100">
-                      how are you
-                    </div>
-                  </div>
-                </li>
-                <li className="mb-3 ml-6">
-                  <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#00d1e848] rounded-full z-10">
-                    <img
-                      className="w-full rounded-full h-full shadow-lg"
-                      src="http://localhost:3000/images/admin.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-3 bg-slate-100 rounded-lg border border-slate-200 shadow-sm">
-                    <div className="flex justify-between items-center mb-2">
-                      <Link className="text-md font-normal">Admin</Link>
-                      <time className="mb-1 text-sm font-normal sm:order-last sm:mb-0">
-                        4 day ago
-                      </time>
-                    </div>
-                    <div className="p-2 text-xs font-normal bg-slate-200 rounded-lg border border-slate-100">
-                      how are you
-                    </div>
-                  </div>
-                </li>
+                  </li>
+                ))}
               </ol>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full p-4  bg-white rounded-md shadow mt-6">
+      <div className="w-full p-4  bg-[#283046] rounded-md mt-6">
         <div className="flex justify-between items-center">
-          <h2 className="font-semibold text-lg text-gray-900 pb-3">
+          <h2 className="font-semibold text-lg text-[#d0d2d6] pb-3">
             Recent Orders
           </h2>
-          <Link className="font-semibold text-sm text-gray-900">View All</Link>
+          <Link className="font-semibold text-sm text-[#d0d2d6]">View All</Link>
         </div>
         <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-left text-gray-900">
-            <thead className="text-sm text-gray-800 uppercase border-b border-slate-700">
+          <table className="w-full text-sm text-left text-[#d0d2d6]">
+            <thead className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
               <tr>
                 <th scope="col" className="py-3 px-4">
                   Order Id
@@ -252,37 +243,39 @@ const AdminDashboard = () => {
               </tr>
             </thead>
             <tbody>
-              {[1, 2, 3, 4, 5].map((d, i) => (
+              {recentOrders.map((d, i) => (
                 <tr key={i}>
                   <td
                     scope="row"
-                    className="py-3 px-4 font-medium whitespace-nowrap text-gray-600"
+                    className="py-3 px-4 font-medium whitespace-nowrap"
                   >
-                    #455fdf54545
+                    #{d._id}
                   </td>
                   <td
                     scope="row"
-                    className="py-3 px-4 font-medium whitespace-nowrap text-gray-600"
+                    className="py-3 px-4 font-medium whitespace-nowrap"
                   >
-                    $656
+                    ${d.price}
                   </td>
                   <td
                     scope="row"
-                    className="py-3 px-4 font-medium whitespace-nowrap text-gray-600"
+                    className="py-3 px-4 font-medium whitespace-nowrap"
                   >
-                    <span>pending</span>
+                    <span>{d.delivery_status}</span>
                   </td>
                   <td
                     scope="row"
-                    className="py-3 px-4 font-medium whitespace-nowrap text-gray-600"
+                    className="py-3 px-4 font-medium whitespace-nowrap"
                   >
-                    <span>pending</span>
+                    <span>{d.payment_status}</span>
                   </td>
                   <td
                     scope="row"
-                    className="py-3 px-4 font-medium whitespace-nowrap text-gray-600"
+                    className="py-3 px-4 font-medium whitespace-nowrap"
                   >
-                    <Link>view</Link>
+                    <Link to={`/admin/dashboard/order/details/${d._id}`}>
+                      view
+                    </Link>
                   </td>
                 </tr>
               ))}
