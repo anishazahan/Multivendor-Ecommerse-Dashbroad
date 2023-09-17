6a2edecb3eac19e5f55e36268ba7e1ca3b7ea858
anishazahan <anishazahan13@gmail.com>
2023-09-17 17:18:41 +0600

    show product details

--- src/views/pages/admin/OrderDetails.jsx
+++ src/views/pages/admin/OrderDetails.jsx
@@ -2,44 +2,53 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  admin_order_status_update,
+  get_admin_order,
+  messageClear,
+} from "../../../Store/Reducers/orderReducer";
 // import { get_admin_order, admin_order_status_update, messageClear } from '../../store/Reducers/OrderReducer'
 const OrderDetails = () => {
   const { orderId } = useParams();
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  // const { order, errorMessage, successMessage } = useSelector(state => state.order)
+  const { order, errorMessage, successMessage } = useSelector(
+    (state) => state.order
+  );
 
-  // useEffect(() => {
-  //     dispatch(get_admin_order(orderId))
-  // }, [orderId])
+  useEffect(() => {
+    dispatch(get_admin_order(orderId));
+  }, [orderId]);
 
-  // const [status, setStatus] = useState('')
-  // useEffect(() => {
-  //     setStatus(order?.delivery_status)
-  // }, [order])
-  // const status_update = (e) => {
-  //     dispatch(admin_order_status_update({ orderId, info: { status: e.target.value } }))
-  //     setStatus(e.target.value)
-  // }
+  const [status, setStatus] = useState("");
+  useEffect(() => {
+    setStatus(order?.delivery_status);
+  }, [order]);
+  const status_update = (e) => {
+    dispatch(
+      admin_order_status_update({ orderId, info: { status: e.target.value } })
+    );
+    setStatus(e.target.value);
+  };
 
-  // useEffect(() => {
-  //     if (successMessage) {
-  //         toast.success(successMessage)
-  //         dispatch(messageClear())
-  //     }
-  //     if (errorMessage) {
-  //         toast.error(errorMessage)
-  //         dispatch(messageClear())
-  //     }
-  // }, [successMessage, errorMessage])
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+    }
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+  }, [successMessage, errorMessage]);
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="w-full p-4  bg-white shadow rounded-md">
         <div className="flex justify-between items-center p-4">
           <h2 className="text-xl text-[#d0d2d6]">Order Details</h2>
           <select
-            // onChange={status_update}
-            // value={status}
+            onChange={status_update}
+            value={status}
             name=""
             id=""
             className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]"
@@ -53,22 +62,21 @@ const OrderDetails = () => {
         </div>
         <div className="p-4">
           <div className="flex gap-2 text-lg text-[#d0d2d6]">
-            {/* <h2>#{order._id}</h2>
-            <span>{order.date}</span> */}
+            <h2>#{order._id}</h2>
+            <span>{order.date}</span>
           </div>
           <div className="flex flex-wrap">
             <div className="w-[32%]">
               <div className="pr-3 text-[#d0d2d6] text-lg">
                 <div className="flex flex-col gap-1">
                   <h2 className="pb-2 font-semibold">
-                    Deliver to :{/* {order.shippingInfo?.name} */}
+                    Deliver to :{order.shippingInfo?.name}
                   </h2>
                   <p>
                     <span className="text-sm">
-                      shipping info
-                      {/* ${order.shippingInfo?.address}{" "}
+                      shipping info ${order.shippingInfo?.address}{" "}
                       {order.shippingInfo?.province} {order.shippingInfo?.city}{" "}
-                      {order.shippingInfo?.area} */}
+                      {order.shippingInfo?.area}
                     </span>
                   </p>
                 </div>
@@ -76,13 +84,13 @@ const OrderDetails = () => {
                   <h2>Payment Status : </h2>
                   <span className="text-base">
                     pending
-                    {/* {order.payment_status} */}
+                    {order.payment_status}
                   </span>
                 </div>
-                <span>Price : ${/* {order.price} */}</span>
+                <span>Price : ${order.price}</span>
                 <div className="mt-4 flex flex-col gap-8">
                   <div className="text-[#d0d2d6]">
-                    {/* {order.products &&
+                    {order.products &&
                       order.products.map((p, i) => (
                         <div key={i} className="flex gap-3 text-md">
                           <img
@@ -101,7 +109,7 @@ const OrderDetails = () => {
                             </p>
                           </div>
                         </div>
-                      ))} */}
+                      ))}
                   </div>
                 </div>
               </div>
@@ -109,7 +117,7 @@ const OrderDetails = () => {
             <div className="w-[68%]">
               <div className="pl-3">
                 <div className="mt-4 flex flex-col">
-                  {/* {order?.suborder?.map((o, i) => (
+                  {order?.suborder?.map((o, i) => (
                     <div key={i + 20} className="text-[#d0d2d6] mb-6">
                       <div className="flex justify-start items-center gap-3">
                         <h2>Seller {i + 1} order : </h2>
@@ -135,7 +143,7 @@ const OrderDetails = () => {
                         </div>
                       ))}
                     </div>
-                  ))} */}
+                  ))}
                 </div>
               </div>
             </div>
