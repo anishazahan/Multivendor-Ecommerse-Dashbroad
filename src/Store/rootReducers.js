d47eb9b58d1f4146fb1595ffb886466211259f68
anishazahan <anishazahan13@gmail.com>
2023-09-18 00:51:25 +0600

    dashbroad data show for admin

--- src/Store/rootReducers.js
+++ src/Store/rootReducers.js
@@ -1,6 +1,7 @@
 import authReducer from "./Reducers/authReducer";
 import categoryReducer from "./Reducers/categoryReducer";
 import chatReducer from "./Reducers/chatReducer";
+import dashboardIndexReducer from "./Reducers/dashboardIndexReducer";
 import OrderReducer from "./Reducers/orderReducer";
 import PaymentReducer from "./Reducers/paymentReducer";
 import productReducer from "./Reducers/productReducer";
@@ -14,5 +15,6 @@ const rootReducer = {
   chat: chatReducer,
   order: OrderReducer,
   payment: PaymentReducer,
+  dashboardIndex: dashboardIndexReducer,
 };
 export default rootReducer;
