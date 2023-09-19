a8b8cbe1abc895638c6967d853d0dcc546c9b0ef
anishazahan <anishazahan13@gmail.com>
2023-09-20 00:37:22 +0600

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+// const local = "http://localhost:5000";
+const local = "https://multivendor-backend-eight.vercel.app";
 const api = axios.create({
-  baseURL: "api",
+  baseURL: `${local}/api`,
 });
 export default api;
