fda40be8e2294c656ae08d56e95b083a39863536
anishazahan <anishazahan13@gmail.com>
2023-09-19 20:20:05 +0600

    api update

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,4 +8,4 @@ export const overrideStyle = {
   alignItems: "center",
 };
 
-export const socket = io("http://localhost:5000");
+export const socket = io("https://multivendor-backend-eight.vercel.app");
